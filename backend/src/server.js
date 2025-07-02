ce39d1343c97a057d42e3f8d082e185b8b2d38db
“Alexis <hayleytheblossomkitty@gmail.com>
2025-07-02 12:35:33 -0400

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,15 +10,25 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(
-    cors({
-        origin: "http://localhost:5173"
-    })
-);
 
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173"
+        })
+    );
+}
 app.use("/api/users", userRoutes);
 app.use("/api/stories", storyRoutes);
 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(5001, (req, res) => {
