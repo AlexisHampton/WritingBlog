fee9f0dded945b59b4a61a33af218826d8c0ef6b
“Alexis <121771464+AlexisHampton@users.noreply.github.com>
2025-08-19 12:15:04 -0400

    prepared for redeploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,6 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json());
-
 if (process.env.NODE_ENV !== "production") {
     app.use(
         cors({
@@ -21,9 +19,22 @@ if (process.env.NODE_ENV !== "production") {
         })
     );
 }
+app.use(express.json());
+
+//To deal with CORS issue
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+
+    next();
+});
+
+
 app.use("/api/users", userRoutes);
 app.use("/api/stories", storyRoutes);
-
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
